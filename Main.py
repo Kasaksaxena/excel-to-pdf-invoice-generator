e12c4fd1c717c7a52a0ef278aa2dd9a328653ac2
Kasaksaxena <kasaksaxena6@gmail.com>
2024-10-11 21:58:00 +0530

    Create a minimal pdf for each excel file

--- Main.py
+++ Main.py
@@ -2,10 +2,24 @@ import pandas as pd
 # glob module is use to retrieve files or dic
 #that matches a specipic pattern
 import glob
+from fpdf import FPDF 
+from pathlib import Path
+
 
 filepaths=glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
     df=pd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)
+    #Create a pdf object with potrait orientation
+    pdf=FPDF(orientation="P",unit="mm",format="A4")
+    # Add a new page 
+    pdf.add_page()
+    filename=Path(filepath).stem
+    invoice_nr=filename.split("-")[0]
+    pdf.set_font(family="Times",size=18,style="B")
+    pdf.cell(w=60, h=10,txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"Pdfs/{filename}.pdf")
+    
+    
+    
     
